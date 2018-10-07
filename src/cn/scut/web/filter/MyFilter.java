7cde5a3ed92468e93b02c76b8cf520213f891504
russul <706937503@qq.com>
2018-10-07 11:06:41 +0800

    first commit

---
+++ src/cn/scut/web/filter/MyFilter.java
@@ -0,0 +1,53 @@
+package cn.scut.web.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class MyFilter
+ */
+
+public class MyFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public MyFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		System.out.println("MyFilter#start");
+		//放行到下一个过滤器或者目标资源servlet
+		chain.doFilter(request, response);
+		System.out.println("MyFilter#end");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
